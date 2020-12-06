cba20511852a1d8b48aa42afd8533de95de3a000
jeff <jeffhogg86@gmail.com>
2020-12-05 21:39:54 -0800

    add scripts, styling

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -32,27 +32,21 @@ export default class DataArea extends Component {
 
     const compareFnc = (a, b) => {
       if (this.state.order === "ascend") {
-        // account for missing values
         if (a[heading] === undefined) {
           return 1;
         } else if (b[heading] === undefined) {
           return -1;
-        }
-        // numerically
-        else if (heading === "name") {
+        } else if (heading === "name") {
           return a[heading].first.localeCompare(b[heading].first);
         } else {
           return a[heading] - b[heading];
         }
       } else {
-        // account for missing values
         if (a[heading] === undefined) {
           return 1;
         } else if (b[heading] === undefined) {
           return -1;
-        }
-        // numerically
-        else if (heading === "name") {
+        } else if (heading === "name") {
           return b[heading].first.localeCompare(a[heading].first);
         } else {
           return b[heading] - a[heading];
@@ -67,7 +61,6 @@ export default class DataArea extends Component {
     console.log(event.target.value);
     const filter = event.target.value;
     const filteredList = this.state.users.filter((item) => {
-      // merge data together, then see if user input is anywhere inside
       let values = Object.values(item).join("").toLowerCase();
       return values.indexOf(filter.toLowerCase()) !== -1;
     });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../styles/Header.css";
 
 export default class Header extends Component {
@@ -6,8 +6,7 @@ export default class Header extends Component {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
       </div>
-    )
+    );
   }
 }
